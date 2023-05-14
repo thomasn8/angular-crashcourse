65817f06b4816883a304bfe6e3da78164827bec3
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-05-14 18:17:50 +0200

    with routing

--- front/project/src/app/app-routing.module.ts
+++ front/project/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { LearningPageComponent } from './components/learning-page/learning-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: TasksComponent },
+	{ path: 'learning-page', component: LearningPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true}) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
